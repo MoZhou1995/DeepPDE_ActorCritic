f171bbbd8f330ff54d70e28ca8d9f910da1e5f99
MoZhou1995 <31834863+MoZhou1995@users.noreply.github.com>
2020-08-03 17:00:40 -0400

    Now LQR in 5d with discount works

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ import equation as eqn
 from solver import ActorCriticSolver
 
 
-flags.DEFINE_string('config_path', 'configs/lqr_d2.json',
+flags.DEFINE_string('config_path', 'configs/lqr_d10.json',
                     """The path to load json file.""")
-flags.DEFINE_string('exp_name', 'LQR2dADMM3NN',
+flags.DEFINE_string('exp_name', 'LQR10dADMM3NN',
                     """The name of numerical experiments, prefix for logging""")
 FLAGS = flags.FLAGS
 FLAGS.log_dir = './logs'  # directory where to write event logs and output array
@@ -61,26 +61,26 @@ def main(argv):
             training_history,x,y, true_y, z, true_z = ActorCritic_solver.train()
             # loss_act[index] = loss_actor
         #print("lmbd", lmbd, "loss_actor", np.mean(loss_act))
-    r = np.sqrt(np.sum(np.square(x), 1, keepdims=False))
-    theta = np.arctan(x[:,1]/x[:,0])
-    theta2 = np.arctan(z[:,1]/z[:,0])
-    theta3 = np.arctan(true_z[:,1]/true_z[:,0])
-    u_norm = np.sqrt(np.sum(np.square(z), 1, keepdims=False))
-    # print(x,y)
-    f1 = plt.figure()
-    ax1 = f1.add_subplot(111)
-    ax1.plot(r,y,'ro',label='value_r_V')
-    plt.legend()
-    f2 = plt.figure()
-    ax2 = f2.add_subplot(111)
-    ax2.plot(r,u_norm,'ro', label='control_r_|u|')
-    plt.legend()
-    f3 = plt.figure()#for angle of actor
-    ax3 = f3.add_subplot(111)
-    ax3.plot(theta,theta2,'ro', label='control_angle')
-    ax3 = f3.add_subplot(111)
-    ax3.plot(theta,theta3,'bo', label='true_angle')
-    plt.legend()
+    # r = np.sqrt(np.sum(np.square(x), 1, keepdims=False))
+    # theta = np.arctan(x[:,1]/x[:,0])
+    # theta2 = np.arctan(z[:,1]/z[:,0])
+    # theta3 = np.arctan(true_z[:,1]/true_z[:,0])
+    # u_norm = np.sqrt(np.sum(np.square(z), 1, keepdims=False))
+    # # print(x,y)
+    # f1 = plt.figure()
+    # ax1 = f1.add_subplot(111)
+    # ax1.plot(r,y,'ro',label='value_r_V')
+    # plt.legend()
+    # f2 = plt.figure()
+    # ax2 = f2.add_subplot(111)
+    # ax2.plot(r,u_norm,'ro', label='control_r_|u|')
+    # plt.legend()
+    # f3 = plt.figure()#for angle of actor
+    # ax3 = f3.add_subplot(111)
+    # ax3.plot(theta,theta2,'ro', label='control_angle')
+    # ax3 = f3.add_subplot(111)
+    # ax3.plot(theta,theta3,'bo', label='true_angle')
+    # plt.legend()
     #print(np.sign(x[:,0]/u[:,0]))
     # f4 = plt.figure() # for critic
     # temp = (x[:,0])**2 - (x[:,1])**2
