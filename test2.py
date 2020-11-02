340fcf2d58e26431e6d214a728d562e9291e23ba
MoZhou1995 <31834863+MoZhou1995@users.noreply.github.com>
2020-11-02 11:34:22 -0500

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 """
-This code is to test a controled SDE dX_t = u*X_t dt + d W_t in 2d
+This code is to test a controled SDE dX_t = u*X_t dt + sqrt(2) d W_t in 2d
 on the unit circle. Let tau be the stopping time that X_t exit the 
 circle. We want to compute the stopping time with killed diffusion
 algorithm, which means that we need to solve a quartic equation. 
@@ -12,27 +12,120 @@ method to train it.
 import numpy as np
 import tensorflow as tf
 from scipy.stats import multivariate_normal as normal
-
+tf.keras.backend.set_floatx("float64")
 # parameters
 total_time = 1.0
 dim = 2
 num_interval = 2
 num_sample = 32
+R = 1.0
+gamma = 1.0
+sigma = np.sqrt(2.0)
+
+# function for boundary
+def b_np(x): #num_sample * 1
+    return np.sum(x**2, 1, keepdims=True) - (R**2)
+def b_tf(x): #num_sample * 1
+    return tf.reduce_sum(x**2, 1, keepdims=True) - (R**2)
 
 # sample x0 and dw, 
 def sample(num_sample, dim, T, N):
     print("call sample")
-    return 0, 0
+    delta_t = T / N
+    sqrt_delta_t = np.sqrt(delta_t)
+    x0 = np.zeros(shape=[0, dim])
+    while np.shape(x0)[0] < num_sample:
+        x_Sample = np.random.uniform(low=-R, high=R, size=[num_sample*dim,dim])
+        index = np.where(b_np(x_Sample) < 0)
+        x0 = np.concatenate([x0, x_Sample[index[0],:]], axis=0)
+        # print(np.shape(x0)[0])
+        if np.shape(x0)[0] > num_sample:
+            x0 = x0[0:num_sample,:]
+    dw_sample = normal.rvs(size=[num_sample,dim,N]) * sqrt_delta_t
+    return x0, dw_sample
 
 # SDE solver, return x_smp and coef, this is the key point to test
 def propagate(num_sample, dim, x0, dw_sample, T, N):
-    return 0, 0
+    delta_t = T / N
+    x_smp = tf.reshape(x0, [num_sample, dim, 1])
+    x_i = x0
+    flag = np.ones([num_sample])
+    for i in range(N):
+        delta_x_drift = Solver.model.u * x_i * delta_t
+        delta_x_diffusion = sigma * dw_sample[:, :, i]
+        delta_x = delta_x_drift + delta_x_diffusion
+        x_iPlus1_temp = x_i + delta_x
+        Exit = b_tf(x_iPlus1_temp) #Exit>=0 means out
+        Exit = tf.reshape(tf.math.ceil((tf.math.sign(Exit)+1)/2), [num_sample])
+        coef_i_temp = (1 - Exit) * flag
+        exit_index = tf.where(flag*Exit==1) # care the dim, num_exit x 1
+        num_exit = tf.shape(exit_index)[0]
+        def no_exit():
+            return coef_i_temp
+        def have_exit():
+            x_exit =tf.gather_nd(x_i,exit_index)
+            delta_x_drift_exit = tf.gather_nd(delta_x_drift,exit_index)
+            delta_x_diffusion_exit = tf.gather_nd(delta_x_diffusion,exit_index)
+             # the following code is to explicitly solve a quartic equation [a,b,c,d,e]*[x^4,x^3,x^2,x,1]=0 to get sqrt_rho
+            # we want to find rho s.t. x_i + drift * rho + diffusion * sqrt_rho is on the boundary
+            a = tf.reduce_sum(delta_x_drift_exit**2, axis=1, keepdims=False) # shape = [num_sample] for a b c d e p q Delta_0 Delta_1
+            b = 2 * tf.reduce_sum(delta_x_drift_exit * delta_x_diffusion_exit, axis=1, keepdims=False)
+            c = tf.reduce_sum(2 * delta_x_drift_exit * x_exit + (delta_x_diffusion_exit**2), axis=1, keepdims=False)
+            d = 2 * tf.reduce_sum(delta_x_diffusion_exit * x_exit, axis=1, keepdims=False)
+            e = tf.reduce_sum(x_exit**2, axis=1, keepdims=False) - R**2
+            p = (8*a*c - 3*(b**2)) / (8*(a**2))
+            q = (b**3 - 4*a*b*c + 8*(a**2)*d) / (8 * (a**3))
+            sign_q = tf.sign(q)
+            Delta_0 = c**2 - 3*b*d + 12*a*e
+            Delta_1 = 2*(c**3) - 9*b*c*d + 27*(b**2)*e + 27*a*(d**2) - 72*a*c*e
+            Delta_2 = Delta_1**2 - 4*(Delta_0**3)
+            sign_Delta_2 = tf.sign(Delta_2)
+            signal_Delta_2 = tf.math.ceil((sign_Delta_2+1)/2)
+            # this is for sign_Delta >= 0
+            QQ = (Delta_1 + ( tf.abs(Delta_2) )**0.5 ) / 2 # the absolute value here is to make sure that no nan
+            Q = tf.sign(QQ) * tf.abs(QQ)**(1/3) #I don't know why python cannot compute the cubic root of negative number
+            S_plus = 0.5 * tf.abs((Q + Delta_0/Q) / (3*a) - 2*p/3)**0.5
+            # This is for sign_Delta < 0
+            phi = tf.math.acos( tf.math.minimum( tf.abs(Delta_1**2 / 4/ Delta_0**3)**0.5,1) )
+            S_minus = 0.5 * tf.abs(2 * (tf.abs(Delta_0)**0.5) * tf.math.cos(phi/3) / (3*a) - 2*p/3)**0.5
+            S = signal_Delta_2 * S_plus + (1-signal_Delta_2) * S_minus
+            # either root1 or root3 is what we want.
+            # root1 = 0.5 * (q/S - 4*(S**2) - 2*p)**0.5 - b/4/a - S
+            # root2 = - 0.5 * (q/S - 4*(S**2) - 2*p)**0.5 - b/4/a - S
+            # root3 = 0.5 * (-q/S - 4*(S**2) - 2*p)**0.5 - b/4/a + S
+            # root4 = - 0.5 * (-q/S - 4*(S**2) - 2*p)**0.5 - b/4/a + S
+            # root = tf.concat([[root1],[root2],[root3],[root4]],axis=0)
+            # root = tf.transpose(root)
+            temp = -4*(S**2) -2*p + tf.abs(q/S)
+            sqrt_rho = 0.5 * tf.abs(temp)**0.5 - b/4/a - sign_q*S
+            check_index = tf.where( (1-sqrt_rho) * sqrt_rho < 0)
+            def every_point_good():
+                return sqrt_rho
+            def some_point_not_good():
+                new_temp = -4*(S**2) -2*p - tf.abs(q/S)
+                new_sqrt_rho = 0.5 * tf.abs(new_temp)**0.5 - b/4/a + sign_q*S # new_temp should be positive at check_index
+                new_sqrt_rho = tf.gather(new_sqrt_rho, check_index[:,0])
+                return tf.tensor_scatter_nd_update(sqrt_rho, check_index, new_sqrt_rho)
+            sqrt_rho_final = tf.cond(tf.shape(check_index)[0] > 0, some_point_not_good, every_point_good)
+            rho = sqrt_rho**2
+            return tf.tensor_scatter_nd_update(coef_i_temp, exit_index, rho)
+        coef_i = tf.cond(num_exit>0, have_exit, no_exit)
+        if i==0:
+            coef = tf.reshape(coef_i, [num_sample, 1])
+        else:
+            coef = tf.concat([coef, tf.reshape(coef_i, [num_sample, 1])], axis=1)
+            # print(tf.reduce_sum(coef_i))
+            # print(tf.reduce_sum(x_i))
+            x_i = x_i + delta_x_drift * tf.reshape(coef_i, [num_sample,1]) + delta_x_diffusion * tf.reshape(coef_i**0.5, [num_sample,1])
+            x_smp = tf.concat([x_smp, tf.reshape(x_i, [num_sample, dim, 1])], axis=2)
+            flag = flag * (1 - Exit)
+    return x_smp, coef
 
 class solver(object):
     def __init__(self, total_time, dim):
         self.T = total_time
         self.dim = dim
-        self.lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay([10], [1e-1,1e-2])
+        self.lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay([6], [1e-1,1e-2])
         # define the model
         self.model = model(total_time, dim)
         # define Adam optimizer
@@ -46,18 +139,18 @@ class solver(object):
             print("u", self.model.u.numpy())
 
     def grad(self, num_sample, x0, dw_sample, T, N):
-        print("call grad")
+        # print("call grad")
         with tf.GradientTape(persistent=True) as tape:
-            loss = self.model(x0, dw_sample) + self.model.u**2
-        print("trainable variable", self.model.trainable_variables)
+            loss = self.model(x0, dw_sample) #+ self.model.u**2
+        # print("trainable variable", self.model.trainable_variables)
         Grad = tape.gradient(loss, self.model.trainable_variables)
-        print("Grad",Grad)
+        # print("Grad",Grad)
         del tape
         return Grad
     #define training
     @tf.function
     def train_step(self, num_sample, x0, dw_sample, T, N):
-        print("call train_step")
+        # print("call train_step")
         gradient = self.grad(num_sample, x0, dw_sample, T, N)
         self.optimizer.apply_gradients(zip(gradient, self.model.trainable_variables))
     
@@ -65,13 +158,27 @@ class model(tf.keras.Model):
     def __init__(self, total_time, dim):
         super(model, self).__init__()
         # the trainable variable u
-        self.u = tf.Variable(initial_value = [1.0], trainable=True)
+        self.u = tf.Variable(initial_value = 1.0, trainable=True, dtype="float64")
+        print(self.u)
         self.T = total_time
         self.dim = dim
+        self.N = num_interval
+        self.num_sample = num_sample
+        self.gamma = gamma
         
     def call(self, x0, dw):
+        x, coef = propagate(self.num_sample, self.dim, x0, dw, self.T, self.N)
         # compute the running cost, which is also the loss function
-        return 0
+        delta_t = self.T / self.N
+        y = 0
+        discount = 1 #broadcast to num_sample x 1
+        for t in range(self.N):
+            # running cost
+            w = (self.u**2 + 2) * tf.reduce_sum((x[:,:,t])**2, 1, keepdims=True) - 2 * self.dim
+            y = y + coef[:,t:t+1] * w * delta_t * discount
+            discount *= tf.math.exp(-self.gamma * delta_t * coef[:,t:t+1])
+        y = y + discount * tf.reduce_sum((x[:,:,-1])**2, 1, keepdims=True) # add the true value
+        return y #num_sample x dim
     
 # the following is the main function
 Solver = solver(total_time, dim)    
