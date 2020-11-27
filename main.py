fe5b86149e10bd2f0496bf47ca01089a9645c19d
MoZhou1995 <31834863+MoZhou1995@users.noreply.github.com>
2020-11-27 11:57:16 -0500

    lqr_d10

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ import matplotlib.pyplot as plt
 import equation as eqn
 from solver import ActorCriticSolver
 
-flags.DEFINE_string('config_path', 'configs/lqr_d2.json',
+flags.DEFINE_string('config_path', 'configs/lqr_d10.json',
                     """The path to load json file.""")
-flags.DEFINE_string('exp_name', 'lqr_d2',
+flags.DEFINE_string('exp_name', 'lqr_d10',
                     """The name of numerical experiments, prefix for logging""")
 FLAGS = flags.FLAGS
 FLAGS.log_dir = './logs'  # directory where to write event logs and output array
@@ -57,34 +57,35 @@ def main(argv):
     ActorCritic_solver = ActorCriticSolver(config, bsde)
     training_history,x,y, true_y, z, true_z, grad_y = ActorCritic_solver.train()
     
-    r = np.sqrt(np.sum(np.square(x), 1, keepdims=False))
-    theta = np.arctan(x[:,1]/x[:,0])
-    theta2 = np.arctan(z[:,1]/z[:,0])
-    theta3 = np.arctan(true_z[:,1]/true_z[:,0])
-    theta4 = np.arctan(grad_y[:,1]/grad_y[:,0])
-    u_norm = np.sqrt(np.sum(np.square(z), 1, keepdims=False))
-    grad_y_norm = np.sqrt(np.sum(np.square(grad_y), 1, keepdims=False))
-    f1 = plt.figure()
-    ax1 = f1.add_subplot(111)
-    ax1.plot(r,y,'ro',label='value_r_V')
-    ax1.plot(r,true_y,'bo', label='true_value')
-    plt.legend()
-    f2 = plt.figure()
-    ax2 = f2.add_subplot(111)
-    ax2.plot(r,u_norm,'ro', label='control_r_|u|')
-    plt.legend()
-    f3 = plt.figure()#for angle of actor
-    ax3 = f3.add_subplot(111)
-    ax3.plot(theta,theta2,'ro', label='control_angle')
-    ax3 = f3.add_subplot(111)
-    ax3.plot(theta,theta3,'bo', label='true_angle')
-    plt.legend()
-    f4 = plt.figure()
-    plt.quiver(x[:,0],x[:,1],z[:,0],z[:,1])
-    plt.legend(["control field"])
-    f5 = plt.figure()
-    plt.quiver(x[:,0],x[:,1],true_z[:,0],true_z[:,1])
-    plt.legend(["true control field"])
+    # r = np.sqrt(np.sum(np.square(x), 1, keepdims=False))
+    # theta = np.arctan(x[:,1]/x[:,0])
+    # theta2 = np.arctan(z[:,1]/z[:,0])
+    # theta3 = np.arctan(true_z[:,1]/true_z[:,0])
+    # theta4 = np.arctan(grad_y[:,1]/grad_y[:,0])
+    # u_norm = np.sqrt(np.sum(np.square(z), 1, keepdims=False))
+    # grad_y_norm = np.sqrt(np.sum(np.square(grad_y), 1, keepdims=False))
+    # f1 = plt.figure()
+    # ax1 = f1.add_subplot(111)
+    # ax1.plot(r,y,'ro',label='value_r_V')
+    # ax1.plot(r,true_y,'bo', label='true_value')
+    # plt.legend()
+    # f2 = plt.figure()
+    # ax2 = f2.add_subplot(111)
+    # ax2.plot(r,u_norm,'ro', label='control_r_|u|')
+    # plt.legend()
+    # f3 = plt.figure()#for angle of actor
+    # ax3 = f3.add_subplot(111)
+    # ax3.plot(theta,theta2,'ro', label='control_angle')
+    # ax3 = f3.add_subplot(111)
+    # ax3.plot(theta,theta3,'bo', label='true_angle')
+    # plt.legend()
+    # f4 = plt.figure()
+    # plt.quiver(x[:,0],x[:,1],z[:,0],z[:,1])
+    # plt.legend(["control field"])
+    # f5 = plt.figure()
+    # plt.quiver(x[:,0],x[:,1],true_z[:,0],true_z[:,1])
+    # plt.legend(["true control field"])
+    
     char = sample+"_"+scheme+"_"+TD+"_"+train
     np.savetxt('{}_{}.csv'.format(path_prefix,char),
                training_history,
